c552a1325a93ccd9ab026e3e20cb1660dc41c01b
Leon <leonwheeler08@gmail.com>
2021-05-07 07:25:33 +0100

    Created template for testing engineer

---
+++ __tests__/engineer.test.js
@@ -0,0 +1,9 @@
+const Engineer = require("../lib/engineer");
+
+describe("Engineer's details", () => {
+  it("should return the inputted name", {});
+  it("should return the inputted id", {});
+  it("should return the inputted email", {});
+  it("should return the inputted github link", {});
+  it("should return the role", {});
+});
