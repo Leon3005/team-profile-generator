98cf6666ebb5334c24119ad0fbefa37317c0f025
Leon <leonwheeler08@gmail.com>
2021-05-07 20:38:32 +0100

    Creating test for engineer name

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -1,9 +1,19 @@
 const Engineer = require("../lib/engineer");
 
 describe("Engineer's details", () => {
-  it("should return the inputted name", {});
-  it("should return the inputted id", {});
-  it("should return the inputted email", {});
-  it("should return the inputted github link", {});
-  it("should return the role", {});
+  it.only("should return the inputted name", () => {
+    const engineer1 = new Engineer(
+      "Leon",
+      "1722",
+      "leonwheeler08@gmail.com",
+      "Leon3005"
+    );
+    const expected = "Leon";
+    const actual = engineer1.name;
+    expect(actual).toEqual(expected);
+  });
+  it("should return the inputted id", () => {});
+  it("should return the inputted email", () => {});
+  it("should return the inputted github link", () => {});
+  it("should return the role", () => {});
 });
