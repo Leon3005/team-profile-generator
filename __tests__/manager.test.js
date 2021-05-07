061e5c0c9a533c421e739babe32f5dad15ca299e
Leon <leonwheeler08@gmail.com>
2021-05-07 21:01:59 +0100

    Manager inputs tested to match a static name/email etc

---
+++ __tests__/manager.test.js
@@ -0,0 +1,31 @@
+const Manager = require("../lib/manager");
+
+const manager1 = new Manager("John", "749", "john@john.com", 9182928383);
+
+describe("Manager's details", () => {
+  it("should return the inputted name", () => {
+    const expected = "John";
+    const actual = manager1.name;
+    expect(actual).toEqual(expected);
+  });
+  it("should return the inputted id", () => {
+    const expected = "749";
+    const actual = manager1.id;
+    expect(actual).toEqual(expected);
+  });
+  it("should return the inputted email", () => {
+    const expected = "john@john.com";
+    const actual = manager1.email;
+    expect(actual).toEqual(expected);
+  });
+  it("should return the inputted office number", () => {
+    const expected = 9182928383;
+    const actual = manager1.officeNumber;
+    expect(actual).toEqual(expected);
+  });
+  it("should return the role", () => {
+    const expected = "Manager";
+    const actual = manager1.getRole();
+    expect(actual).toEqual(expected);
+  });
+});
