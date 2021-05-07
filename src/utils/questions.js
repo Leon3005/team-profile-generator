4ccab8933c49225610770dcd3b4a134fab83cca5
Leon <leonwheeler08@gmail.com>
2021-05-07 20:56:57 +0100

    Created manager questions

--- src/utils/questions.js
+++ src/utils/questions.js
@@ -1,3 +1,25 @@
-const questions [
+const questions = [
+  {
+    type: "input",
+    message: "Please enter the manager name:",
+    name: "managerName",
+  },
+  {
+    type: "input",
+    message: "Please enter the manager's ID:",
+    name: "managerId",
+  },
+  {
+    type: "input",
+    message: "Please enter the manager's email address:",
+    name: "managerEmail",
+  },
+  {
+    type: "input",
+    message: "Please enter the office number:",
+    name: "managerOfficeNum",
+  },
+];
 
-]
+// Exporting the questions array to be used in another file.
+module.exports = questions;
