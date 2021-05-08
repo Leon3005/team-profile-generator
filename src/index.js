e7d02a8a44a7663631ea607f3f4607d3625b27b5
Leon <leonwheeler08@gmail.com>
2021-05-08 08:55:06 +0100

    Testing creating a HTML file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const questions = require("./utils/questions");
+
+const questionResponses = async (questions) => {
+  const answers = await inquirer.prompt(questions);
+  return answers;
+};
+
+const writeHTML = () => {
+  const callback = (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Created Team Profile!");
+    }
+  };
+  fs.writeFile("./utils/TestHTML.html", `hi`, "utf8", callback);
+};
+
+writeHTML();
